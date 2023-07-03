49f3c3096fb79b76190617996c6cd96e8c22e108
Elodie Billiot <elodie.billiot@prestashop.com>
2023-07-03 14:11:10 +0200

    Feat: integration translators

--- terraform/envs/integration/main.tf
+++ terraform/envs/integration/main.tf
@@ -13,5 +13,5 @@ module "api" {
   labels                      = local.labels
   image                       = "europe-west1-docker.pkg.dev/ps-translators-integration/translators/translators"
   app_version                 = var.app_version
-  endpoint_host               = "translators-integration.prestashop.com"
+  endpoint_host               = "translators-integration2.prestashop.com"
 }
