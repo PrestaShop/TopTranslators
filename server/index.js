f7b68f2448935daf477fa982bfdd685ef66a7274
Pablo Borowicz <pablo.borowicz@prestashop.com>
2023-08-21 12:51:13 +0200

    Support build using Node 16

--- server/index.js
+++ server/index.js
@@ -1,41 +1,43 @@
-const Inert = require('inert');
-const Hapi = require('hapi');
-const server = new Hapi.Server();
+const Inert = require('@hapi/inert');
+const Hapi = require('@hapi/hapi');
+const server = new Hapi.Server({
+  port: 3000,
+});
 
-server.register(Inert, () => {});
-server.connection({ port: 3000 });
+const provision = async () => {
+  await server.register(Inert);
 
-server.ext('onPreResponse', (request, reply) => {
-    if (request.response.isBoom) {
-        return reply.redirect('/');
+  server.ext('onPreResponse', (request, reply) => {
+    if (request.response.isBoom && !request.response.isServer) {
+      return reply.redirect('/');
     }
-    return reply.continue();
-});
+    return reply.continue;
+  });
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: (request, reply) => {
-    reply.file('../public/index.html', { confine: false });
-  }
-});
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+      return reply.file('../public/index.html', {confine: false});
+    }
+  });
 
-server.route({
-  method: 'GET',
-  path: '/{file*}',
-  handler: (request, reply) => {
-    const parameters = request.params.file.split('/');
-    let path = '../public/';
-    parameters.forEach((value) => {
-      path += '/'+encodeURIComponent(value);
-    });
-    reply.file(path, { confine: false });
-  }
-});
+  server.route({
+    method: 'GET',
+    path: '/{file*}',
+    handler: (request, reply) => {
+      const parameters = request.params.file.split('/');
+      let path = '../public/';
+      parameters.forEach((value) => {
+        path += '/' + encodeURIComponent(value);
+      });
+      return reply.file(path, {confine: false});
+    }
+  });
 
-server.start((err) => {
-  if (err) {
-    throw err;
-  }
+  await server.start();
+
   console.log('Server running at port 3000');
-});
+}
+
+provision();
